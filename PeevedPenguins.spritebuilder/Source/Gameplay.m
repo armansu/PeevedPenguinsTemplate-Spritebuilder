9dd14052077b370257961d8f0daefd54e9c1f034
Arman Suleimenov <arman@collections.me>
2014-07-10 20:15:06 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -14,6 +14,8 @@
     CCNode * _levelNode;
     CCNode * _contentNode;
     CCNode * _pullbackNode;
+    CCNode * _mouseJointNode;
+    CCPhysicsJoint * _mouseJoint;
 }
 
 - (void) retry {
@@ -28,10 +30,37 @@
     
     _physicsNode.debugDraw = TRUE;
     _pullbackNode.physicsBody.collisionMask = @[];
+    
+    _mouseJointNode.physicsBody.collisionMask = @[];
 }
 
 - (void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    [self launchPenguin];
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    if(CGRectContainsPoint([_catapultArm boundingBox], touchLocation)) {
+        _mouseJointNode.position = touchLocation;
+        _mouseJoint  = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34,138) restLength:0.f stiffness:3000.f damping:150.f];
+    }
+}
+
+- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event {
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
+}
+
+- (void) releaseCatapult {
+    if(_mouseJoint != nil) {
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+    }
+}
+
+- (void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self releaseCatapult];
+}
+
+- (void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self releaseCatapult];
 }
 
 - (void) launchPenguin {
